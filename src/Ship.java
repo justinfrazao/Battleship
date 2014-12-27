c7e171fcb3b64811383a0061f2a81a37ca611365
justinfrazao <justinfrazao@gmail.com>
2014-12-26 22:33:58 -0500

    Finished Ship Class

--- src/Ship.java
+++ src/Ship.java
@@ -4,7 +4,13 @@ public class Ship {
 	public Point firstCoordinate;
 	public Point secondCoordinate;
 	public Ship(Point a, Point b){
-		firstCoordinate = a;
-		secondCoordinate = b;
+		if (a.x == b.x || a.y == b.y) {	
+			firstCoordinate = a;
+			secondCoordinate = b;
+		}
+		else {
+			System.out.println("ERROR!!!");
+		}
+		
 	}
 }

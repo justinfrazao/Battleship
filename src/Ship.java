97ab4dad4c2e5fc8fceddc37c14dd10b45bbcd19
Justin Frazao <justinfrazao@Justins-MacBook-Pro.local>
2014-12-26 22:13:50 -0500

    Added Ship class

---
+++ src/Ship.java
@@ -0,0 +1,10 @@
+import java.awt.Point;
+
+public class Ship {
+	public Point firstCoordinate;
+	public Point secondCoordinate;
+	public Ship(Point a, Point b){
+		firstCoordinate = a;
+		secondCoordinate = b;
+	}
+}
